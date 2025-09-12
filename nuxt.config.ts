98df4702bef3fa707ef96cda4bf9e997209ebe23
Byron Ferguson <ferguson.bs@gmail.com>
2025-09-12 15:40:25 -0500

    adding PWA and GH Pages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,61 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
+  ssr: false,
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint', '@nuxt/ui', '@nuxt/test-utils'],
+  modules: [
+    '@nuxt/eslint',
+    '@nuxt/ui',
+    '@nuxt/test-utils',
+    [
+      '@vite-pwa/nuxt',
+      {
+        registerType: 'autoUpdate',
+        manifest: {
+          name: 'Lorcana Lore Tracker',
+          short_name: 'Lore Tracker',
+          description: 'Track Lore for Lorcana games offline!',
+          theme_color: '#18181b',
+          background_color: '#18181b',
+          display: 'standalone',
+          start_url: '/',
+          icons: [
+            {
+              src: '/favicon.ico',
+              sizes: '64x64 32x32 24x24 16x16',
+              type: 'image/x-icon',
+            },
+          ],
+        },
+        workbox: {
+          globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
+          runtimeCaching: [
+            {
+              urlPattern: /^https:\/\/.*/,
+              handler: 'NetworkFirst',
+              options: {
+                cacheName: 'external-cache',
+                expiration: {
+                  maxEntries: 50,
+                  maxAgeSeconds: 7 * 24 * 60 * 60,
+                },
+              },
+            },
+            {
+              urlPattern: /^\/(.*)/,
+              handler: 'NetworkFirst',
+              options: {
+                cacheName: 'local-cache',
+                expiration: {
+                  maxEntries: 200,
+                  maxAgeSeconds: 7 * 24 * 60 * 60,
+                },
+              },
+            },
+          ],
+        },
+      },
+    ],
+  ],
   css: ['~/assets/css/main.css'],
 });
